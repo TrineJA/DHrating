db28fce9dfcbc1a7cbacc75ecec06857fd8629d3
Trine Abrahamsen <Trine.Abrahamsen@maersk.com>
2019-09-10 19:39:34 +0200

    introduce course length as input scaling factir in tabs with relative ratings

--- src/app/TACI_diff.py
+++ src/app/TACI_diff.py
@@ -3,7 +3,7 @@ import dash_core_components as dcc
 import dash_table
 from dash.dependencies import Input, Output
 
-from data_utilities import wide_to_long
+from data_utilities import wide_to_long, get_rating_columns
 
 from app import app
 
@@ -11,17 +11,33 @@ import plotly.graph_objs as go
 
 #### THIS IS THE ONLY DIFFERENCE BETWEEN THE APPS
 from data import df_circle_diff as df
+ws_cols = get_rating_columns(df)
 
 # define layout
 layout = html.Div([
     html.H3("Time difference to our boat. Negative is faster"),
-    html.Div(id='f_ratings_reactive_container_taci_diff', style={'marginBottom': 30}),
+
+    html.Div([
+        html.P("Length of course in Nautical Miles")
+        ], style = {'display': 'inline-block', 'width': '270px'}
+    ),
+
+    html.Div([
+        dcc.Input(
+            id='input_nm',
+            placeholder='Nm',
+            debounce = True,
+            type='text',
+            value='1.0',
+            style={'width': 70}
+        )
+        ], style = {'display': 'inline-block', 'width': '30%', 'margin':'auto'}
+    ),
 
     html.Div([
         dash_table.DataTable(id='t_ratings_reactive_taci_diff',
                              columns=
                              [{'name': 'BoatKey', 'id': 'BoatKey', 'type': 'text'}] + [{"name": i, "id": i, 'type':'numeric'} for i in df.columns.drop('BoatKey')],
-                             data=df.to_dict('records'),
                              sort_action="native",
                              row_selectable="multi",
                              selected_rows=[],
@@ -42,8 +58,28 @@ layout = html.Div([
                                  'fontWeight': 'bold'
                              },
                              ),
-    ], style={'marginBottom': 30})
+    ], style={'marginBottom': 30}),
+
+    html.H3("Time difference to our boat per Nautical Mile"),
+
+    html.Div(id='f_ratings_reactive_container_taci_diff', style={'marginBottom': 30}),
+
 ])
+
+# update table based on input distance
+@app.callback(
+    Output('t_ratings_reactive_taci_diff', 'data'),
+    [Input('input_nm', 'value')])
+def update_table(scaling_factor):
+    # if scaling factor is empty set it to 1
+    if not scaling_factor:
+        scaling_factor = '1'
+
+    #df_tmp introduced to avoid scaling is relative to what is currently in table
+    df_tmp = df.copy()
+    df_tmp[ws_cols] = round(df_tmp[ws_cols] * float(scaling_factor), 1)
+    return df_tmp.to_dict('records')
+
 
 # make graph for selected rows
 @app.callback(
